fb67abcc513b0a861584de44feb74bafbb7324b8
Olivier Drees <olivier.s.drees@gmail.com>
2020-04-09 21:28:40 +0200

    test commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'Njk3MTQyMDA4ODY2OTMwODU4.XozQQA.5B1h5Ax_R2iDGxlpXOUKzc88KGk';
+
+const PREFIX = '!';
+
+bot.on('ready', () =>{
+    console.log('Bot online!!');
+})
+
+bot.on('message', message=>{
+   
+    let args = message.content.substring(PREFIX.length).split(" ")
+
+    switch(args[0]){
+        case 'ping':
+            message.channel.send('pong je kanker oma!');
+            break;
+        case 'merch':
+            message.channel.send('Oh shit hoorde ik daar merch, ja dat hoorde ik goed. Ga nu naar https://knolpower.nl/ en haal je kanker vette merchandise van Enzo Kanker.');
+            break;
+        case 'knolpower':
+            message.channel.send("JAAAA BROEDER KNOLPOWER");
+            break;
+    }
+})
+
+bot.on('message', message=>{
+
+    if(message.content === "enzoknol is geen leuk kanaal"){
+        message.reply('Zomaar lieg jij verschrikkelijke faggot.');
+    }else if(message.content === "osama"){
+        message.channel.send("ALLAHUAKBAR!!");
+    }
+})
+
+bot.login(token);
